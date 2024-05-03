73fd1aca88c455dd2614e12acd74269a5a0ea4cf
ericktek <erickbale360@gmail.com>
2024-05-03 22:39:04 +0300

    first commit

---
+++ app/(navbar)/login/page.jsx
@@ -0,0 +1,12 @@
+import LogIn from '@/app/components/Login'
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+        <LogIn />
+    </div>
+  )
+}
+
+export default login
---
+++ app/components/Register.jsx
@@ -0,0 +1,63 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Register = () => {
+  return (
+    <form className='border p-4 rounded-md mt-3'>
+          <div className="relative flex items-center mt-4 ">
+                <span className="absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
+                </span>
+
+                <input type="text" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11   focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Username"/>
+            </div>
+
+           
+            <div className="relative flex items-center mt-6">
+                <span className="absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                    </svg>
+                </span>
+
+                <input type="email" className="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address"/>
+            </div>
+
+            <div className="relative flex items-center mt-4">
+                <span className="absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                    </svg>
+                </span>
+
+                <input type="password" className="block w-full px-10 py-3  bg-white border rounded-lg  dark:text-gray-300 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Password"/>
+            </div>
+
+            <div className="relative flex items-center mt-4">
+                <span className="absolute">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                    </svg>
+                </span>
+
+                <input type="password" className="block w-full px-10 py-3 border rounded-lg focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Confirm Password"/>
+            </div>
+
+            <div className="mt-6">
+                <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                    Sign Up
+                </button>
+
+                <div className="mt-6 text-center ">
+                    <p className='text-sm'>
+                        Already have an account? <Link href="login" className="cursor-pointer text-blue-500 hover:underline dark:text-blue-400">Log In</Link>
+                    </p>
+                </div>
+            </div>
+    </form>
+  )
+}
+
+export default Register
