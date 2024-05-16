0cb0a90d6a7740eff3d5636c0d92ac931d50e214
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2024-05-17 00:56:37 +0300

    remove code that is not compatible with edge and separate them

--- auth.js
+++ auth.js
@@ -1,43 +1,31 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import { authConfig } from "./auth.config";
-import { compare } from "bcryptjs";
-import User from "./app/models/user";
-import { connectMongoDB } from "./app/db/mongodb";
-
-async function login(email) {
-  try {
-    await connectMongoDB();
-    const user = await User.findOne({ email: email });
-    return user;
-  } catch (error) {
-    throw new Error("Failed to login");
-  }
-}
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 
 export const { signIn, signOut, auth } = NextAuth({
   ...authConfig,
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        try {
-          const { email, password } = credentials;
-          const user = await login(email);
-          if (!user) {
-            return null;
-          } else {
-            const isValid = await compare(password, user?.password);
-            if (isValid) {
-              return user;
-            } else {
-              return null;
-            }
-          }
-        } catch (error) {
-          console.error("Authentication error:", error);
-          return null;
-        }
-      },
-    }),
-  ],
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.name = token.name;
+      }
+      return session;
+    },
+
+    async signIn(user, account, profile) {
+      return true;
+    },
+    async redirect(url, baseUrl) {
+      return baseUrl;
+    },
+    async session(session, user) {
+      session.user = user;
+      return session;
+    },
+  },
 });
